55986afbffddd290d0751223138168d55174198a
Aduda-Boaz <adudaboaz@gmail.com>
2021-05-28 00:14:13 +0300

    Add the prompt feature in the main

--- bin/main.rb
+++ bin/main.rb
@@ -1,1 +1,22 @@
-puts 'Hello World!'
+require_relative '../lib/page_info'
+
+def prompt
+  puts "Welcome to the most trusted football information center \n \n"
+  puts "To continue \n Press 'y' \n \n"
+  puts "To quit \n Type 'q' \n \n"
+  loop do
+    text_input = gets.chomp.downcase
+    if ['y', ''].include?(text_input)
+      @page_info += 1
+      break
+    elsif %w[q].include?(text_input)
+      puts 'Thanks for trusting our source!'.blue.bold
+      puts 'Thank you for your time!'.green.bold
+      exit
+    else
+      puts 'Sorry! Please enter a valid character \n\n!'.red.bold
+      puts 'Press \n 'y' to continue \n \n'
+    end
+    
+  end
+end
