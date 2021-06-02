70d88c7bf9a2e9ed69fcbd20dd8639198775d5a7
Aduda-Boaz <adudaboaz@gmail.com>
2021-06-02 10:15:13 +0300

    Code refactor for the scraper method

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,35 +1,18 @@
 require 'nokogiri'
-require 'watir'
-require 'rubocop'
-require 'rspec'
+require 'httparty'
+
 
 # Scraper class
 class Scraper
-  attr_reader :link
-
-  def initialize(link)
-    @link = link
+  def read(file_name, arr, match)
+    File.read(file_name, arr.join("\n"))
+    puts '#{file_name} created in the root directory with #{arr.length - 1} #{match}.'
   end
-
-  def begin
-    url = @link
-    browser = ::Watir::Browser.new
-    browser.goto(url)
-    browser.element(css: 'li.ais-InfiniteHits-item').wait_until(&:present)
-    doc = browser.element(css: '#rendered-content')
-    html_raw = doc.inner_html
-    parsed_page = Nokodiri::HTML(html_raw)
-    matches_list = parsed_page.css('div.card-content')
-    pages(parsed_page, matches_list)
-  end
-
+  
   private
 
-  def pages(parsed_page = nil, matches_list = nil)
-    @page = 1
-    per_page = matches_list.count
-    per_page = 1 if per_page.zero?
-    @max = parsed_page.css('h2.rc-NumberOfResultsSection span').text.gsub(/[^\d]/, '').to_i
-    @last_page = (@max / per_page)
+  def parsing_page(url)
+    Nokogiri::HTML(HTTParty.get(url).body)
   end
+  
 end
